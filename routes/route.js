162c07c96f32f815da4a3642c795b3b032475a75
emma-mathieu <emma.mathieu@etu.umontpellier.fr>
2023-03-16 11:58:19 +0100

    suppression jeux

--- routes/route.js
+++ routes/route.js
@@ -1,15 +1,9 @@
 module.exports = (app) =>{
-    const jeuRouter = require('./jeu_route');
-    const typeJeuRouter = require('./typeJeu_route');
     const zoneRouter = require('./zone_route');
     const utilisateurRouter = require('./utilisateur_route');
     const attributionZoneRouter = require('./attributionZone_route');
-    const attributionJeuRouter = require('./attributionJeu_route');
 
-    app.use('/jeux', jeuRouter);
-    app.use('/typeJeux', typeJeuRouter);
     app.use('/zones', zoneRouter);
     app.use('/utilisateurs', utilisateurRouter);
     app.use('/attributionsZone', attributionZoneRouter);
-    app.use('/attributionsJeux', attributionJeuRouter);
 }
