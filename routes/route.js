b6dfbbb00e8b702697ab9089fb603be8c442c114
EAgnese <agneseeri@gmail.com>
2023-03-17 09:32:44 +0100

    début crud affectationCreneauJour

--- routes/route.js
+++ routes/route.js
@@ -1,15 +1,18 @@
 module.exports = (app) =>{
     const zoneRouter = require('./zone_route');
     const utilisateurRouter = require('./utilisateur_route');
-    const attributionZoneRouter = require('./attributionZone_route');
     const festivalRouter = require('./festival_route');
     const jourRouter = require('./jour_route');
     const creneauRouter = require('./creneau_route');
+    const affectationBenevoleCreneauRouter = require('./attributionZone_route');
+    const affectationCreneauJourRouter = require('./attributionZone_route');
 
     app.use('/zones', zoneRouter);
     app.use('/utilisateurs', utilisateurRouter);
-    app.use('/attributionsZone', attributionZoneRouter);
     app.use('/festivals', festivalRouter);
     app.use('/jours', jourRouter);
     app.use('/creneaux', creneauRouter);
+    app.use('/affectationBC', affectationBenevoleCreneauRouter);
+    app.use('/affectationCJ', affectationCreneauJourRouter);
+    
 }
