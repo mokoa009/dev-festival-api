582e35fb4f914b548f8f92b22ba70f97b67cb879
EAgnese <agneseeri@gmail.com>
2023-03-17 09:18:15 +0100

    modif typo

--- routes/jour_route.js
+++ routes/jour_route.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const jour_controller = require("../controllers/jour_controller")
 
-router.get("/",jour_controller.selectZones)
-router.get("/:id",jour_controller.selectZoneById)
-router.delete("/delete",jour_controller.deleteZone)
-router.post("/create",jour_controller.createZone)
-router.put("/update",jour_controller.updateZoneById)
+router.get("/",jour_controller.selectJours)
+router.get("/:id",jour_controller.selectJourById)
+router.delete("/delete",jour_controller.deleteJour)
+router.post("/create",jour_controller.createJour)
+router.put("/update",jour_controller.updateJourById)
+
 
 module.exports = router
