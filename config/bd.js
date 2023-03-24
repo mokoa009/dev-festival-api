58d80a3e7da789e50f3b8f8ed9050348464e69e1
emma-mathieu <emma.mathieu@etu.umontpellier.fr>
2023-03-24 15:08:54 +0100

    config deploiement

--- config/bd.js
+++ config/bd.js
@@ -2,7 +2,7 @@ let mysql = require('mysql2');
 require("dotenv").config()
 const url = require('url');
 
-const dbUrl = process.env.DATABASE_URL; // dsn l
+const dbUrl = process.env.DATABASE_URL; // dsn le
 const params = url.parse(dbUrl);
 
 const connection = mysql.createPool({
